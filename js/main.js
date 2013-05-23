49fec083f6b1fec7d56a291f3739da30bc81854f
Marcos Hernández <marcoslhc@gmail.com>
2013-05-23 15:59:40 -0400

    App entry point.
    config.js configures require and kicks off the app. main is the entry
    point of the app. From here, the rest is up to the developer decision
    and the app requirement.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-
+define(['jquery','underscore','backbone'], function($,_,Backbone){
+    return function(){
+        console.log('jQuery:\t\t'+$.fn.jquery);
+        console.log('Underscore:\t'+_.VERSION);
+        console.log('Backbone:\t'+Backbone.VERSION);
+        console.log('Hello World');
+    }
+});
